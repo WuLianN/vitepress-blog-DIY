956b026bda038fd1f45ec3fe3b6863bb9fe5fce0
wuliann <610575024@qq.com>
2021-05-20 14:21:17 +0800

    refactory(Home.vue): 解耦，将最新文章模块封装为组件

---
+++ app/.vitepress/theme/components/LatestArticle.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="flex flex-col items-center justify-around">
+    <div
+      class="lastest-title text-left text-green-500 text-lg w-11/12 md:w-160"
+    >
+      最新文章
+    </div>
+    <div
+      class="
+        lasteset-box
+        flex flex-col
+        justify-between
+        border-2
+        shadow-xl
+        rounded-2xl
+        w-11/12
+        h-52
+        md:w-160
+        md:h-60
+      "
+      v-for="(item, index) in pages"
+      :key="index"
+    >
+      <Card :data="item">
+        <template #tag>
+          <div class="absolute bottom-2 left-0">
+            <button
+              class="
+                mr-6
+                p-0.5
+                text-white text-sm
+                rounded-sm
+                bg-green-500
+                active:bg-green-700
+                ring-4 ring-green-500 ring-opacity-50
+              "
+              v-for="item in item.frontMatter.tag"
+              :key="item"
+              @click.stop="goTag(item)"
+            >
+              {{ item }}
+            </button>
+          </div>
+        </template>
+
+        <template #date>
+          <div class="absolute bottom-0 right-0 text-sm lg:text-base">
+            {{ item.frontMatter.date }}
+          </div>
+        </template>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Card from './Card.vue'
+import { defineProps } from 'vue'
+import { useRouter } from 'vitepress'
+
+const props = defineProps({
+  pages: Array,
+})
+
+const router = useRouter()
+
+const goTag = (tag) => {
+  router.go(`/tag#${tag}`)
+}
+</script>
+
+<style scoped>
+.lastest-title {
+  margin: 20px 0;
+}
+
+.lasteset-box {
+  padding: 20px;
+  background: white;
+  margin: 0 0 20px 0;
+}
+</style>
--- app/.vitepress/theme/views/Home.vue
+++ app/.vitepress/theme/views/Home.vue
@@ -1,65 +1,14 @@
 <template>
   <div class="home">
-    <div class="flex flex-col items-center justify-around">
-      <div
-        class="lastest-title text-left text-green-500 text-lg w-11/12 md:w-160"
-      >
-        最新文章
-      </div>
-      <div
-        class="
-          lasteset-box
-          flex flex-col
-          justify-between
-          border-2
-          shadow-xl
-          rounded-2xl
-          w-11/12
-          h-52
-          md:w-160
-          md:h-60
-        "
-        v-for="(item, index) in pages"
-        :key="index"
-      >
-        <Card :data="item">
-          <template #tag>
-            <div class="absolute bottom-2 left-0">
-              <button
-                class="
-                  mr-6
-                  p-0.5
-                  text-white text-sm
-                  rounded-sm
-                  bg-green-500
-                  active:bg-green-700
-                  ring-4 ring-green-500 ring-opacity-50
-                "
-                v-for="item in item.frontMatter.tag"
-                :key="item"
-                @click.stop="goTag(item)"
-              >
-                {{ item }}
-              </button>
-            </div>
-          </template>
-
-          <template #date>
-            <div class="absolute bottom-0 right-0 text-sm lg:text-base">
-              {{ item.frontMatter.date }}
-            </div>
-          </template>
-        </Card>
-      </div>
-    </div>
+    <LatestArticle :pages="pages" />
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
-import { useRouter, useSiteData } from 'vitepress'
+import { useSiteData } from 'vitepress'
 import lodash from 'lodash'
-import Card from '../components/Card.vue'
+import LatestArticle from '../components/LatestArticle.vue'
 
 const siteData = useSiteData()
 
@@ -73,12 +22,6 @@ const queryList = {
 }
 
 pages.value.push(...pageList.slice(queryList.page, queryList.limit))
-
-const router = useRouter()
-
-const goTag = (tag) => {
-  router.go(`/tag#${tag}`)
-}
 
 const handleScroll = () => {
   const scrollTop = Math.floor(document.documentElement.scrollTop)
@@ -117,15 +60,5 @@ onUnmounted(() => {
 <style scoped>
 .home {
   width: 100%;
-}
-
-.lastest-title {
-  margin: 20px 0;
-}
-
-.lasteset-box {
-  padding: 20px;
-  background: white;
-  margin: 0 0 20px 0;
 }
 </style>
